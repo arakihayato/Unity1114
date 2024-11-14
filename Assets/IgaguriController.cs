cab99f63ad693b0c8ecbbba0721d93318a18c459
408-18\student <fca>
2024-11-14 19:18:11 +0900

    発射したイガグリを時間経過で消滅させた

--- Assets/IgaguriController.cs
+++ Assets/IgaguriController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class IgaguriController : MonoBehaviour
 {
+    public bool Hit = false;
+    public int Time = 0;
+    public int MaxTime = 60;
     public void Shoot(Vector3 dir)
     {
         GetComponent<Rigidbody>().AddForce(dir);
@@ -12,6 +15,25 @@ public class IgaguriController : MonoBehaviour
     void Start()
     {
         Application.targetFrameRate = 60;
-        Shoot(new Vector3(0, 200, 2000));
+        //Shoot(new Vector3(0, 200, 2000));
+    }
+
+    private void Update()
+    {
+       if (Hit == true)
+        {
+            Time++;
+            if (Time >= MaxTime)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        GetComponent<Rigidbody>().isKinematic = true;
+        GetComponent<ParticleSystem>().Play();
+        Hit = true;
     }
 }
